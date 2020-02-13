1fa16090115b404368fde23e5d8a03e460e6bba1
Catherine De Meulemeester <de_meulemeester_c@hotmail.com>
2020-02-13 10:01:49 +0100

    create search component

--- video-app/src/App.js
+++ video-app/src/App.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import './App.css';
 import Nav from './Components/Nav';
+import SearchBox from './Components/SearchBox';
 import Home from './View/Home';
 
 import Card from './Components/Card';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import Search from './Components/SearchBox';
 
 function App() {
   return (
     <Router>
     <div className="App">
      <Nav></Nav>
+     <Search></Search>
      <Switch>
      <Route path="/" exact component={Home}></Route>
      
